70de799e1dc4d4f411aab6e1aa0cc52fd3a732df
Dan <Dan@10.77.0.141>
2018-09-19 08:40:35 +0300

    day3

---
+++ Day3/src/Name.java
@@ -0,0 +1,9 @@
+
+public class Name {
+	static String first = "Theo";
+	static String last = "Moose";
+	static String full = first + " " + last;
+	public static void main(String args[]) {
+	System.out.println(full);
+	}
+}
---
+++ Day3/src/Swan.java
@@ -0,0 +1,10 @@
+
+public class Swan {
+	int numberEggs;
+	public static void main(String[] args) {
+		Swan mother = new Swan();
+		mother.numberEggs = 5;
+		System.out.println(mother.numberEggs);
+	}
+	
+}
